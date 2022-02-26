f3c4deddf5ff673bd87995e8d4ea3dac71015ecb
张豪 <zhanghao@yuduan.net>
2022-02-26 16:32:54 +0800

    增加nuget支持

--- src/obj/Debug/netstandard2.0/Yuduan.Redis.AssemblyInfo.cs
+++ src/obj/Debug/netstandard2.0/Yuduan.Redis.AssemblyInfo.cs
@@ -11,13 +11,17 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Yuduan.Redis")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Yuduan Network Technology Co., Ltd.")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCopyrightAttribute("Yuduan Network")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("a library extends StackExchange.Redis,support Net Framework 4.6.1+ and net Standa" +
+    "rd 2.0+")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Yuduan.Redis")]
+[assembly: System.Reflection.AssemblyProductAttribute("Yuduan Redis Extension")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Yuduan.Redis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/yuduannet/Yuduan.Redis")]
 
 // 由 MSBuild WriteCodeFragment 类生成。
 
